c70521b4823f2a58f2dead19bf067e3037e500fc
Jesof <jesof.jes@gmail.com>
2018-08-25 00:04:29 +0300

    Add trafic sniffer with filter

--- main.go
+++ main.go
@@ -3,11 +3,26 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 )
 
-func main() {
+var (
+	device       string = "wlp2s0"
+	snapshot_len int32  = 1024
+	promiscous   bool   = false
+	err          error
+	timeout      time.Duration = 30 * time.Second
+	handle       *pcap.Handle
+	ethLayer     layers.Ethernet
+	ipLayer      layers.IPv4
+	tcpLayer     layers.TCP
+)
+
+func scanDev() {
 	// Find all devices
 	devices, err := pcap.FindAllDevs()
 	if err != nil {
@@ -23,6 +38,51 @@ func main() {
 		for _, address := range device.Addresses {
 			fmt.Println("- IP address:  ", address.IP)
 			fmt.Println("- Subnet mask:  ", address.Netmask)
+		}
+	}
+}
+
+func main() {
+	scanDev()
+	// Open device
+	handle, err = pcap.OpenLive(device, snapshot_len, promiscous, timeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+
+	// Set filter
+	var filter string = "tcp and port 80"
+	err = handle.SetBPFFilter(filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Only capturing TCP port 80 packets.")
+
+	// Use the handle as a packet source to process all packets
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		parser := gopacket.NewDecodingLayerParser(
+			layers.LayerTypeEthernet,
+			&ethLayer,
+			&ipLayer,
+			&tcpLayer,
+		)
+		foundLayerTypes := []gopacket.LayerType{}
+
+		err := parser.DecodeLayers(packet.Data(), &foundLayerTypes)
+		if err != nil {
+			fmt.Println("Trouble decoding layers: ", err)
+		}
+
+		for _, layerType := range foundLayerTypes {
+			if layerType == layers.LayerTypeIPv4 {
+				fmt.Println("IPv4: ", ipLayer.SrcIP, "->", ipLayer.DstIP)
+			}
+			if layerType == layers.LayerTypeTCP {
+				fmt.Println("TCP Port: ", tcpLayer.SrcPort, "->", tcpLayer.DstPort)
+				fmt.Println("TCP SYN:", tcpLayer.SYN, " | ACK:", tcpLayer.ACK)
+			}
 		}
 	}
 }
